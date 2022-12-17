28ddc18ea49222e02cea2e5810c4fd41b1892d31
”Ning“ <2856167974@qq.com>
2022-12-17 20:23:48 +0800

    初始化

---
+++ src/components/Avatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="avatar-container">
+    <img :src="url" :style="{ width: size + 'px', height: size + 'px' }" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 150,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.avatar-container {
+  border-radius: 50%;
+  object-fit: cover;
+  display: block;
+}
+</style>
